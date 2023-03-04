0e5f648b2ce97a8ca2a49067c15bae21a4df011c
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-03-04 07:59:50 +0000

    initial commit after set up

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const head = () => {
+  return (
+    <>
+        <title>Claire AI Messenger</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </>
+  )
+}
+
+export default head;
---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/outline';
+
+const page = () => {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen px-2 text-white">
+        <h1 className="mb-20 text-5xl font-bold">Claire AI</h1>
+
+        <section className="">
+            <div className="">
+                <div className="flex flex-col items-center justify-center mb-5">
+                    {/* sun icon */}
+                    <SunIcon className="w-8 h-8" />
+                    <h3>Examples</h3>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">"Explain Something to me"</p>
+                    <p className="infoText">"What is the difference between a dog and a cat?"</p>
+                    <p className="infoText">"What is the color of the sun?"</p>
+                </div>
+            </div>
+
+            <div className="">
+                <div className="flex flex-col items-center justify-center mb-5">
+                    {/* sun icon */}
+                    <BoltIcon className="w-8 h-8" />
+                    <h3>Capabilities</h3>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">"Change the OpenAI model to use"</p>
+                    <p className="infoText">"Messages are stored so they can be viewed"</p>
+                    <p className="infoText">"Hot toast notifications when ClaireAI is thinking..."</p>
+                </div>
+            </div>
+
+            <div className="">
+                <div className="flex flex-col items-center justify-center mb-5">
+                    {/* sun icon */}
+                    <ExclamationTriangleIcon className="w-8 h-8" />
+                    <h3>Limitations</h3>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">"May occassionally generate incorect information"</p>
+                    <p className="infoText">"May occassionally produce harmful instructions or biased content"</p>
+                    <p className="infoText">"Limited knowledge of world and events after 2021"</p>
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
+
+export default page;
